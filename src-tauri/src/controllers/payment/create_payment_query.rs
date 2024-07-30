201555671f27b6bc51cb9b353a9c6ebd28964154
idrisaltamimi <idris.al.tamimi.1@gmail.com>
2024-07-30 17:04:27 +0400

    created a logic to update the payment if there is already another payment for visit

--- src-tauri/src/controllers/payment/create_payment_query.rs
+++ src-tauri/src/controllers/payment/create_payment_query.rs
@@ -1,24 +1,52 @@
+use crate::connect::database;
+use crate::structs::payment::{CreatePaymentData, NewPaymentData};
 use anyhow::Result;
 use surrealdb::Response;
-
-use crate::connect::database;
-use crate::structs::payment::CreatePaymentData;
 
 pub async fn create_payment_query(data: CreatePaymentData) -> Result<()> {
     let db = database().await?;
 
-    let sql = format!(
-        "CREATE payment SET 
-            payment_type = $payment_type, 
-            name = $name, 
-            category = $category, 
-            amount = $amount, 
-            payment_method = $payment_method, 
-            pending = $pending, 
-            created_at = time::now(),
-            visit = {};",
+    // Check if there's an existing payment with the specified criteria
+    let check_sql = format!(
+        "SELECT * FROM payment WHERE visit = {} AND pending = true AND name = 'treatment_cost';",
         data.visit_id
     );
+
+    let mut response: Response = db.query(check_sql).await?;
+    println!("response: {:#?}", response);
+    let existing_payment: Vec<NewPaymentData> = response.take(0)?;
+    println!("existing_payment: {:#?}", existing_payment);
+    // Construct the SQL string conditionally
+    let sql = if existing_payment.len() > 0 {
+        let payment_id = &existing_payment[0].id.id;
+        format!(
+            "UPDATE payment SET 
+                payment_type = $payment_type, 
+                name = $name, 
+                category = $category, 
+                amount = $amount, 
+                payment_method = $payment_method, 
+                pending = $pending,
+                created_at = time::now()
+            WHERE id = payment:{};",
+            payment_id.to_string()
+        )
+    } else {
+        format!(
+            "CREATE payment SET 
+                payment_type = $payment_type, 
+                name = $name, 
+                category = $category, 
+                amount = $amount, 
+                payment_method = $payment_method, 
+                pending = $pending, 
+                created_at = time::now(),
+                visit = {};",
+            data.visit_id
+        )
+    };
+    println!("sql: {:#?}", sql);
+    // Execute the SQL query
     let _response: Response = db
         .query(sql)
         .bind(("payment_type", data.payment_type))
