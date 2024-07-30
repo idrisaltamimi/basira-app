30dd9bc64b2b93e3de2787b8bff44f43d8f93f2b
idrisaltamimi <idris.al.tamimi.1@gmail.com>
2024-07-30 22:07:43 +0400

    added update visitor query

--- src/hooks/useVisitor.ts
+++ src/hooks/useVisitor.ts
@@ -9,6 +9,16 @@ type VisitorData = {
   gender: string
 }
 
+type UpdateVisitorData = {
+  id: String
+  name: String
+  phone: number
+  civil_id?: number
+  gender: String
+  birthdate: Date
+  file_number: number
+}
+
 export default function useVisitor() {
   const queryClient = useQueryClient()
 
@@ -23,6 +33,27 @@ export default function useVisitor() {
     },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ["get_visits", "get_unpaid_payments"] })
+    }
+  })
+
+  const updateVisitor = useMutation({
+    mutationFn: async (data: UpdateVisitorData) => {
+      try {
+        const res = await invoke("update_visitor", { data })
+        return res
+      } catch (error) {
+        throw new Error(error as string)
+      }
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [
+          "get_visitor",
+          "get_visitors",
+          "get_visits_history",
+          "get_selected_visit"
+        ]
+      })
     }
   })
 
@@ -41,5 +72,5 @@ export default function useVisitor() {
     }
   })
 
-  return { createNewVisit, createVisitor }
+  return { createNewVisit, createVisitor, updateVisitor }
 }
